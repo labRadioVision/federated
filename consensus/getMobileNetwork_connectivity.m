f45bf4fd225d0dbd87d603ea8698c583c83fdd7e
Stefano Savazzi <9395893+labRadioVision@users.noreply.github.com>
2020-02-25 14:56:12 +0100

    Delete getMobileNetwork_connectivity.m

--- consensus/getMobileNetwork_connectivity.m
+++
@@ -1,9 +0,0 @@
-function neighbors = getMobileNetwork_connectivity(ii_saved_local, neighbors, devices, epoch)
-% returns a vector of length 'neighbors' that contains the indexes of
-% neighbors (use NaN if the number of neighbors is lower than 'neighbors'
-neighbors = NaN*ones(neighbors,1);
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
