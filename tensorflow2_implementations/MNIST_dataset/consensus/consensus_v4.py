975dabc278a812e246f3bb4fc6c6c7d417423cff
savazzi <stefano.savazzi@ieiit.cnr.it>
2021-06-07 19:15:33 +0200

    test ge

---
+++ tensorflow2_implementations/MNIST_dataset/consensus/consensus_v4.py
@@ -0,0 +1,226 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+import tensorflow as tf
+import datetime
+import scipy.io as sio
+import math
+import time
+from matplotlib.pyplot import pause
+import os
+import glob
+from tensorflow.keras import layers
+from tensorflow.keras import models
+import warnings
+
+class CFA_process:
+
+    def __init__(self, devices, ii_saved_local, neighbors, federated=True, graph=0):
+        self.federated = federated # true for federation active
+        self.devices = devices # number of devices
+        self.ii_saved_local = ii_saved_local # device index
+        self.neighbors = neighbors # neighbors number (given the network topology)
+        self.graph = graph
+        self.training_end = False
+        if graph == 0:  # use k-degree network
+            self.neighbor_vec = self.get_connectivity(ii_saved_local, neighbors, devices) # neighbor list
+        else:
+            mat_content = self.getMobileNetwork_connectivity(self.ii_saved_local, self.neighbors, self.devices, 0)
+            self.neighbor_vec = np.asarray(mat_content[0], dtype=int)
+
+    def get_neighbor_weights(self, neighbor, epoch_count, eps_t_control, outfile, outfile_models, epoch=0, max_lag=1):
+        warnings.filterwarnings("ignore")
+        success = False
+        # max_lag = 30 # default 30
+        stop_federation = False
+        # neighbor model and stats (train variables)
+        #outfile_models = 'results/dump_train_model{}.npy'.format(neighbor)
+        #outfile = 'results/dump_train_variables{}.npz'.format(neighbor)
+
+        while not os.path.isfile(outfile):
+            print("waiting for variables")
+            pause(1)
+
+        try:
+            dump_vars = np.load(outfile, allow_pickle=True)
+            neighbor_epoch_count = dump_vars['epoch_count']
+            self.training_end = dump_vars['training_end']
+        except:
+            pause(5)
+            print("retrying opening variables")
+            try:
+                dump_vars = np.load(outfile, allow_pickle=True)
+                neighbor_epoch_count = dump_vars['epoch_count']
+                self.training_end = dump_vars['training_end']
+            except:
+                print("halting federation")
+                stop_federation = True
+
+        pause(round(np.random.random(), 2))
+        # check file and updated neighbor frame count, max lag
+        if not stop_federation:
+                while not os.path.isfile(outfile_models) or neighbor_epoch_count < epoch_count - max_lag:
+                    # implementing consensus
+                    # print("neighbor frame {} local frame {}, device {} neighbor {}".format(neighbor_frame_count, frame_count, self.ii_saved_local, neighbor[q]))
+                    pause(1)
+                    try:
+                        dump_vars = np.load(outfile, allow_pickle=True)
+                        neighbor_epoch_count = dump_vars['epoch_count']
+                        self.training_end = dump_vars['training_end']
+                    except:
+                        pause(2)
+                        print("retrying opening variables")
+                        try:
+                            dump_vars = np.load(outfile, allow_pickle=True)
+                            neighbor_epoch_count = dump_vars['epoch_count']
+                            self.training_end = dump_vars['training_end']
+                        except:
+                            print("problems loading variables")
+
+                # load neighbor model
+                try:
+                    neighbor_model = np.load(outfile_models, allow_pickle=True)
+                    success = True
+                except:
+                    pause(5)
+                    print("retrying opening model")
+                    try:
+                        neighbor_model = np.load(outfile_models, allow_pickle=True)
+                        success = True
+                    except:
+                        print("failed to load model federation")
+                        neighbor_model = []
+        else:
+            neighbor_model = []
+
+        return neighbor_model, success
+
+    def getMobileNetwork_connectivity(self, ii_saved_local, neighbors, devices, epoch):
+        graph_index = sio.loadmat('consensus/vGraph.mat')
+        dev = np.arange(1, devices + 1)
+        graph_mobile = graph_index['graph']
+        set = graph_mobile[ii_saved_local, :, epoch]
+        tot_neighbors = np.sum(set, dtype=np.uint8)
+        sets_neighbors_final = np.zeros(tot_neighbors, dtype=np.uint8)
+        counter = 0
+        for kk in range(devices):
+            if set[kk] == 1:
+                sets_neighbors_final[counter] = kk
+                counter = counter + 1
+        return sets_neighbors_final
+
+    def get_connectivity(self, ii_saved_local, neighbors, devices):
+        saved_neighbors = neighbors
+        if neighbors < 2:
+            neighbors = 2 # set minimum to 2 neighbors
+        if (ii_saved_local == 0):
+            sets_neighbors_final = np.arange(ii_saved_local + 1, ii_saved_local + neighbors + 1)
+        elif (ii_saved_local == devices - 1):
+            sets_neighbors_final = np.arange(ii_saved_local - neighbors, ii_saved_local)
+        elif (ii_saved_local >= math.ceil(neighbors / 2)) and (
+                ii_saved_local <= devices - math.ceil(neighbors / 2) - 1):
+            sets_neighbors = np.arange(ii_saved_local - math.floor(neighbors / 2),
+                                       ii_saved_local + math.floor(neighbors / 2) + 1)
+            index_ii = np.where(sets_neighbors == ii_saved_local)
+            sets_neighbors_final = np.delete(sets_neighbors, index_ii)
+        else:
+            if (ii_saved_local - math.ceil(neighbors / 2) < 0):
+                sets_neighbors = np.arange(0, neighbors + 1)
+            else:
+                sets_neighbors = np.arange(devices - neighbors - 1, devices)
+            index_ii = np.where(sets_neighbors == ii_saved_local)
+            sets_neighbors_final = np.delete(sets_neighbors, index_ii)
+
+        if saved_neighbors < 2:
+            neighbors_final = np.delete(sets_neighbors_final, 1)
+        else:
+            neighbors_final = sets_neighbors_final
+
+        return neighbors_final
+
+
+    def federated_weights_computing(self, neighbor, neighbors, epoch_count, eps_t_control, epoch=0, max_lag=30):
+        warnings.filterwarnings("ignore")
+        # max_lag = 30 # default 30
+        stop_federation = False
+        old_weights = self.local_weights
+
+        neighbor_weights = []
+        # seqc = random.sample(range(self.devices), self.active)
+
+        for q in range(neighbors):
+            outfile_models = 'results/dump_train_model{}.npy'.format(neighbor[q])
+            outfile = 'results/dump_train_variables{}.npz'.format(neighbor[q])
+            weight_n, success = self.get_neighbor_weights(neighbor[q], epoch_count, eps_t_control, outfile, outfile_models, epoch=0, max_lag=1)
+            if success:
+                neighbor_weights.append(weight_n)
+            if self.training_end and len(neighbor_weights) > 0:
+                # one of the neighbors solved the optimization, apply transfer learning
+                break
+
+        if len(neighbor_weights) > 0:
+            eps_t_control = 1 / (len(neighbor_weights) + 1) # overwrite
+            for q in range(len(neighbor_weights)):
+                if self.training_end:
+                    print("detected training end")
+                    # it is reasonable to replace local model with the received one as succesful, stop model averaging with other neighbors
+                    for k in range(self.layers):
+                        self.local_weights[k] = neighbor_weights[-1][k]
+                    break
+                else: # apply model averaging
+                    for k in range(self.layers):
+                        self.local_weights[k] = self.local_weights[k] + eps_t_control*(neighbor_weights[q][k]-self.local_weights[k])
+                        # self.local_weights[k] = self.local_weights[k] + eps_t_control * (neighbor_weights[k] - self.local_weights[k])
+            del neighbor_weights
+
+        return self.local_weights.tolist()
+
+    def federated_grads_computing(self, neighbor, neighbors, epoch_count, eps_t_control, epoch=0, max_lag=30):
+        warnings.filterwarnings("ignore")
+        # max_lag = 30 # default 30
+        stop_federation = False
+        old_grads = self.local_gradients
+
+        neighbor_grads = []
+        # seqc = random.sample(range(self.devices), self.active)
+
+        for q in range(neighbors):
+            # neighbor model and stats (train variables)
+            outfile = 'results/dump_train_variables{}.npz'.format(neighbor[q])
+            outfile_models_grad = 'results/dump_train_grad{}.npy'.format(neighbor[q])
+            weight_n, success = self.get_neighbor_weights(neighbor[q], epoch_count, eps_t_control, outfile,
+                                                          outfile_models_grad, epoch=0, max_lag=1)
+            if success:
+                neighbor_grads.append(weight_n)
+            if self.training_end and len(neighbor_grads) > 0:
+                # one of the neighbors solved the optimization, apply transfer learning
+                break
+
+        if len(neighbor_grads) > 0:
+            eps_t_control = 1 / (len(neighbor_grads) + 1)  # overwrite
+            for q in range(len(neighbor_grads)):
+                # apply model averaging
+                for k in range(self.layers):
+                    self.local_gradients[k] = self.local_gradients[k] + eps_t_control * (
+                                    neighbor_grads[q][k] - self.local_gradients[k])
+            del neighbor_grads
+
+        grads_out = []
+        for ii in range(self.layers):
+            grads_out.append(tf.convert_to_tensor(self.local_gradients[ii]))
+
+        return grads_out
+
+    def getTrainingStatusFromNeightbor(self):
+        return self.training_end
+
+    def update_local_target_model(self, model):
+        self.local_weights = model
+        self.layers = self.local_weights.size
+
+    def update_local_gradient(self, gradients):
+        self.local_gradients = gradients
+
+
+    def update_local_model(self, model):
+        self.local_weights = model
+        self.layers = self.local_weights.size
